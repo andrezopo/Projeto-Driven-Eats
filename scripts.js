68e7a7f3b463339ef214e8348faf7dc19e46381e
André Zopolato <andrezopo@gmail.com>
2022-03-31 03:14:04 -0300

    Versão com mensagem no WhatsApp sem preço

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
 let contagemComida = 0;
 let contagemBebida = 0;
-let contagemSobremesa =0;
+let contagemSobremesa = 0;
 let contagemSelecionados = 0;
-function selecionarComida(item){
-    for (i=1 ; i <= 4; i++){
+let precoComida = 0;
+let precoBebida = 0;
+let precoSobremesa = 0;
+function selecionarComida(item) {
+    for (i = 1; i <= 4; i++) {
         document.getElementById('comida' + i).classList.remove('selecionado')
         document.getElementById('vetor-comida' + i).classList.add('invisivel')
     }
@@ -11,14 +14,14 @@ function selecionarComida(item){
     document.getElementById('vetor-' + item.id).classList.remove('invisivel')
     contagemComida = 1;
     contagemSelecionados = (contagemComida + contagemBebida + contagemSobremesa);
-    if (contagemSelecionados === 3){
+    if (contagemSelecionados === 3) {
         document.getElementById('falta-selecionar').classList.add('tudo-selecionado')
         document.getElementById('fechar-pedido').classList.add('pedido-fechado')
     }
 
 }
-function selecionarBebida(item){
-    for (i=1 ; i <= 4; i++){
+function selecionarBebida(item) {
+    for (i = 1; i <= 4; i++) {
         document.getElementById('bebida' + i).classList.remove('selecionado')
         document.getElementById('vetor-bebida' + i).classList.add('invisivel')
     }
@@ -26,13 +29,13 @@ function selecionarBebida(item){
     document.getElementById('vetor-' + item.id).classList.remove('invisivel')
     contagemBebida = 1;
     contagemSelecionados = (contagemComida + contagemBebida + contagemSobremesa);
-    if (contagemSelecionados === 3){
+    if (contagemSelecionados === 3) {
         document.getElementById('falta-selecionar').classList.add('tudo-selecionado')
         document.getElementById('fechar-pedido').classList.add('pedido-fechado')
     }
 }
-function selecionarSobremesa(item){
-    for (i=1 ; i <= 4; i++){
+function selecionarSobremesa(item) {
+    for (i = 1; i <= 4; i++) {
         document.getElementById('sobremesa' + i).classList.remove('selecionado')
         document.getElementById('vetor-sobremesa' + i).classList.add('invisivel')
     }
@@ -40,9 +43,15 @@ function selecionarSobremesa(item){
     document.getElementById('vetor-' + item.id).classList.remove('invisivel')
     contagemSobremesa = 1;
     contagemSelecionados = (contagemComida + contagemBebida + contagemSobremesa);
-    if (contagemSelecionados === 3){
+    if (contagemSelecionados === 3) {
         document.getElementById('falta-selecionar').classList.add('tudo-selecionado')
         document.getElementById('fechar-pedido').classList.add('pedido-fechado')
     }
+}
+let mensagem = "Olá, gostaria de fazer o pedido:\n- Prato: Frango Yin Yang\n- Bebida: Coquinha Gelada\n- Sobremesa: Pudim\nTotal: R$ " + (precoComida + precoBebida + precoSobremesa);
+let mensagemPronta = encodeURIComponent(mensagem);
+function fazerPedido() {
+    alert(mensagemPronta)
+    window.open("https://wa.me/5519998765432?text=" + mensagemPronta)
 }
 
