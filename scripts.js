8872a749ac988b3aad8c2fa484017130f1ca0688
André Zopolato <andrezopo@gmail.com>
2022-03-31 01:35:03 -0300

    Versão com Seleção de itens e Fechar pedido funcionando

--- scripts.js
+++ scripts.js
@@ -1,21 +1,48 @@
+let contagemComida = 0;
+let contagemBebida = 0;
+let contagemSobremesa =0;
 let contagemSelecionados = 0;
 function selecionarComida(item){
+    for (i=1 ; i <= 4; i++){
+        document.getElementById('comida' + i).classList.remove('selecionado')
+        document.getElementById('vetor-comida' + i).classList.add('invisivel')
+    }
     item.classList.add('selecionado')
-    var comida = document.getElementsByClassName('comida')
-    alert(comida)
-    contagemSelecionados += 1
+    document.getElementById('vetor-' + item.id).classList.remove('invisivel')
+    contagemComida = 1;
+    contagemSelecionados = (contagemComida + contagemBebida + contagemSobremesa);
+    if (contagemSelecionados === 3){
+        document.getElementById('falta-selecionar').classList.add('tudo-selecionado')
+        document.getElementById('fechar-pedido').classList.add('pedido-fechado')
+    }
+
 }
 function selecionarBebida(item){
+    for (i=1 ; i <= 4; i++){
+        document.getElementById('bebida' + i).classList.remove('selecionado')
+        document.getElementById('vetor-bebida' + i).classList.add('invisivel')
+    }
     item.classList.add('selecionado')
-    contagemSelecionados += 1
+    document.getElementById('vetor-' + item.id).classList.remove('invisivel')
+    contagemBebida = 1;
+    contagemSelecionados = (contagemComida + contagemBebida + contagemSobremesa);
+    if (contagemSelecionados === 3){
+        document.getElementById('falta-selecionar').classList.add('tudo-selecionado')
+        document.getElementById('fechar-pedido').classList.add('pedido-fechado')
+    }
 }
 function selecionarSobremesa(item){
+    for (i=1 ; i <= 4; i++){
+        document.getElementById('sobremesa' + i).classList.remove('selecionado')
+        document.getElementById('vetor-sobremesa' + i).classList.add('invisivel')
+    }
     item.classList.add('selecionado')
-    contagemSelecionados += 1
-}
-function tudoSelecionado(item){
+    document.getElementById('vetor-' + item.id).classList.remove('invisivel')
+    contagemSobremesa = 1;
+    contagemSelecionados = (contagemComida + contagemBebida + contagemSobremesa);
     if (contagemSelecionados === 3){
-        item.classList.add('tudo-selecionado')
+        document.getElementById('falta-selecionar').classList.add('tudo-selecionado')
+        document.getElementById('fechar-pedido').classList.add('pedido-fechado')
     }
 }
 
