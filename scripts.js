5dc0ff30a1fc55049af66f33d6d48ad5985d4fa0
André Zopolato <andrezopo@gmail.com>
2022-03-31 23:25:02 -0300

    Versão final com formato dos preços corretos na confirmação

--- scripts.js
+++ scripts.js
@@ -77,10 +77,10 @@ let mensagem = "";
 function confirmarPedido(){
     let elemento = document.querySelector(".modal");
     elemento.classList.remove('invisivel');
-    document.querySelector(".primeiro-preco").innerHTML = "R$ " + precoComida.toFixed(2);
-    document.querySelector(".segundo-preco").innerHTML = "R$ " + precoBebida.toFixed(2);
-    document.querySelector(".terceiro-preco").innerHTML = "R$ " + precoSobremesa.toFixed(2);
-    document.querySelector(".preco-total").innerHTML = "R$ " + preco;
+    document.querySelector(".primeiro-preco").innerHTML = "R$ " + precoComida.toFixed(2).replace('.',',');
+    document.querySelector(".segundo-preco").innerHTML = "R$ " + precoBebida.toFixed(2).replace('.',',');
+    document.querySelector(".terceiro-preco").innerHTML = "R$ " + precoSobremesa.toFixed(2).replace('.',',');
+    document.querySelector(".preco-total").innerHTML = "R$ " + preco.replace('.',',');
 }
 function cancelarConfirmacao(){
     let elemento = document.querySelector(".modal");
